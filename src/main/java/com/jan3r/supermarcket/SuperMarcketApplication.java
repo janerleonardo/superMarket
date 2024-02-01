3d661a1110e2240ae2d9a10c2cafc8aa456bfba3
Jan3r <janerleonardo@gmail.com>
2024-01-31 20:53:09 -0500

    first commit

---
+++ src/main/java/com/jan3r/supermarcket/SuperMarcketApplication.java
@@ -0,0 +1,13 @@
+package com.jan3r.supermarcket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperMarcketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperMarcketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jan3r/supermarcket/SuperMarcketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jan3r.supermarcket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperMarcketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
